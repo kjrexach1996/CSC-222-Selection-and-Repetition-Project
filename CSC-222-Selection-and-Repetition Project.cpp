3e5281973a065d06d9fc9a1c92df5ef293e63e1b
Kevin Rexach <kaiorex@hotmail.com>
2024-09-29 16:23:07 -0400

    Selection and Repetition Project Part 1, using nested if and while statements.

--- CSC-222-Selection-and-Repetition Project.cpp
+++ CSC-222-Selection-and-Repetition Project.cpp
@@ -1,20 +1,65 @@
-// CSC-222-Selection-and-Repetition Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This program mimics a calculator by taking integers from a user and performing 
+// an operation of the user's choosing.
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool in_use = true;
+    double operand_1, operand_2;
+    char operation;
+    double result;
+    char answer;
+
+    cout << "Welcome! This program performs an operation between two integers." << endl;
+    while (in_use)
+    {
+        cout << "Please enter the first integer: ";
+        cin >> operand_1;
+        cout << "Please enter the second integer: ";
+        cin >> operand_2;
+        cout << "Please enter the operation to be performed between these integers [+|-|*|/]: ";
+        cin >> operation;
+
+        while (operation != '+' && operation != '-' && operation != '*' && operation != '/')
+        {
+            cout << "ERROR: Invalid input!" << endl;
+            cout << "Please enter the operation to be performed between these integers [+|-|*|/]: ";
+            cin >> operation;
+        }
+        if (operation == '+')
+            result = operand_1 + operand_2;
+        else if (operation == '-')
+            result = operand_1 - operand_2;
+        else if (operation == '*')
+            result = operand_1 * operand_2;
+        else if (operation == '/')
+        {
+            while (operand_2 == 0)
+            {
+                cout << "ERROR: Cannot divide by zero! " << operand_1 << " " << operation << " " << operand_2 << endl;
+                cout << "Choose another denominator: ";
+                cin >> operand_2;
+            }
+            result = operand_1 / operand_2;
+        }
+
+        cout << "Final result: " << operand_1 << " " << operation << " " << operand_2 << " = " << result << endl;
+
+        cout << "Would you like to try again? Enter \'y\' for \"yes\" or enter any character to quit: ";
+        cin >> answer;
+        if (answer == 'y')
+            continue;
+        else
+            in_use = false;
+
+    cout << "Thank you for using my calculator! Bye!";
+    return 0;
+
+    }
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
