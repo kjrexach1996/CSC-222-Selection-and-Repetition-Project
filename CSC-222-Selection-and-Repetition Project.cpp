89d2a935d6741c403554205de61461b69df112ce
Kevin Rexach <kaiorex@hotmail.com>
2024-09-29 19:37:52 -0400

    Selection and Repetition Project Part 3.

--- CSC-222-Selection-and-Repetition Project.cpp
+++ CSC-222-Selection-and-Repetition Project.cpp
@@ -1,61 +1,53 @@
-// 
-//
+// This program takes two text files containing lists of names
+// and merges the names into a separate list in alphabetical order.
 
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 int main()
 {
-    ifstream file_1, file_2;
-    ofstream output_file;
-    string name_1, name_2;
+	ifstream file_1, file_2;
+	ofstream output_file;
+	string name_1, name_2;
 
-    file_1.open("file1.txt");
-    file_2.open("file2.txt");
-    output_file.open("orderedList.txt");
+	file_1.open("file1.txt");
+	file_2.open("file2.txt");
+	output_file.open("final_list.txt");
 
-    getline(file_1, name_1);
-    getline(file_2, name_2);
+	getline(file_1, name_1);
+	getline(file_2, name_2);
 
-    while (!file_1.eof() || !file_2.eof())
-    {
-        
-        if (name_1 < name_2)
-        {
-            output_file << name_1 << "\n";
-            if (getline(file_1, name_1))
-            { 
-                cout << name_1 << endl;
-                continue;
-            }
-                
-        }
-        else
-        {
-            output_file << name_2 << "\n";
-            if (getline(file_2, name_2))
-                continue;
-        }
-            
-    while (!file_1.eof())
-    {
-        output_file << name_1 << "\n";
-        getline(file_1, name_1);
-    }
+	while (!file_1.eof() && !file_2.eof())
+	{
+		if (name_1 < name_2)
+		{
+			output_file << name_1 << endl;
+			getline(file_1, name_1);
+		}
+		else 
+		{
+			output_file << name_2 << endl;
+			getline(file_2, name_2);
+		}
+	}
 
-    while (!file_2.eof())
-    {
-        output_file << name_2 << "\n";
-        getline(file_2, name_2);
-    }
-    
+	while (!file_1.eof())
+	{
+		output_file << name_1 << endl;
+		getline(file_1, name_1);
+	}
 
-    file_1.close();
-    file_2.close();
-    output_file.close();
-    return 0;
+	while (!file_2.eof())
+	{
+		output_file << name_2 << endl;
+		getline(file_2, name_2);
+	}
+
+	file_1.close();
+	file_2.close();
+	output_file.close();
+	return 0;
 }
-
-
