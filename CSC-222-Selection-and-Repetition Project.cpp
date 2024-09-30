080d3784b1bffa3b9f22e0ec52d71625889e38c7
Kevin Rexach <kaiorex@hotmail.com>
2024-09-30 11:21:59 -0400

    Selection and Repetition Project Part 1, using nested if statements.

--- CSC-222-Selection-and-Repetition Project.cpp
+++ CSC-222-Selection-and-Repetition Project.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 int main()
 {
+    //Initialize variables
     bool in_use = true;
-    double operand_1, operand_2;
-    char operation;
-    double result;
-    char answer;
+    double operand_1, operand_2, result;
+    char operation, answer;
 
+    //Loop of calculator working until user exits
     cout << "Welcome! This program performs an operation between two integers." << endl;
     while (in_use)
     {
+        //Asks for integers and operation 
         cout << "Please enter the first integer: ";
         cin >> operand_1;
         cout << "Please enter the second integer: ";
@@ -22,12 +23,15 @@ int main()
         cout << "Please enter the operation to be performed between these integers [+|-|*|/]: ";
         cin >> operation;
 
+        //Checks if inputted operation is valid
         while (operation != '+' && operation != '-' && operation != '*' && operation != '/')
         {
             cout << "ERROR: Invalid input!" << endl;
             cout << "Please enter the operation to be performed between these integers [+|-|*|/]: ";
             cin >> operation;
         }
+
+        //Performs operation
         if (operation == '+')
             result = operand_1 + operand_2;
         else if (operation == '-')
@@ -36,6 +40,7 @@ int main()
             result = operand_1 * operand_2;
         else if (operation == '/')
         {
+            //Error occurs if denominator is zero
             while (operand_2 == 0)
             {
                 cout << "ERROR: Cannot divide by zero! " << operand_1 << " " << operation << " " << operand_2 << endl;
@@ -45,20 +50,18 @@ int main()
             result = operand_1 / operand_2;
         }
 
+        //Display final information and asks if user would like to continue
         cout << "Final result: " << operand_1 << " " << operation << " " << operand_2 << " = " << result << endl;
 
         cout << "Would you like to try again? Enter \'y\' for \"yes\" or enter any character to quit: ";
         cin >> answer;
-        if (answer == 'y')
-            continue;
-        else
+        if (answer != 'y')
             in_use = false;
 
+    }
+    //Program ends
     cout << "Thank you for using my calculator! Bye!";
     return 0;
-
-    }
-
 
 }
 
