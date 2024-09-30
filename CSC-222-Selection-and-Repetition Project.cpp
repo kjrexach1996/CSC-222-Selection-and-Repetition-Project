22c2c816fe26b09485b7827ba2dbbddf6b6d9335
Kevin Rexach <kaiorex@hotmail.com>
2024-09-30 11:31:45 -0400

    Selection and Repetition Project Part 3, merge data in files alphabetically.

--- CSC-222-Selection-and-Repetition Project.cpp
+++ CSC-222-Selection-and-Repetition Project.cpp
@@ -9,17 +9,22 @@ using namespace std;
 
 int main()
 {
+	//Initializes variables
 	ifstream file_1, file_2;
 	ofstream output_file;
 	string name_1, name_2;
 
+	//Opens all files
 	file_1.open("file1.txt");
 	file_2.open("file2.txt");
 	output_file.open("final_list.txt");
 
+	//Gets very first line from each input file file
 	getline(file_1, name_1);
 	getline(file_2, name_2);
 
+	//Loop goes through each name in each file alphabetically and
+	//ends when loop has reached the end of one file.
 	while (!file_1.eof() && !file_2.eof())
 	{
 		if (name_1 < name_2)
@@ -34,18 +39,21 @@ int main()
 		}
 	}
 
+	//Loops in case first file is not finished and enters remaining data
 	while (!file_1.eof())
 	{
 		output_file << name_1 << endl;
 		getline(file_1, name_1);
 	}
 
+	//Loops in case second file is not finished and enters remaining data
 	while (!file_2.eof())
 	{
 		output_file << name_2 << endl;
 		getline(file_2, name_2);
 	}
 
+	//Closes all files
 	file_1.close();
 	file_2.close();
 	output_file.close();
